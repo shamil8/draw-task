cc23acd72b5d82a8d5a4d2ae2dfd6d97292badac
shamil8 <qurbonovshamil@gmail.com>
2022-09-10 16:01:48 +0700

    Add base polygon example

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log('Common parts');
+import fs from 'fs';
+import { createCanvas, } from 'canvas';
+
+const width = 800;
+const height = 600;
+
+const canvas = createCanvas(width, height);
+const context = canvas.getContext('2d');
+
+context.fillStyle = 'blue';
+context.fillRect(0, 0, width, height);
+
+const buffer = canvas.toBuffer('image/png');
+
+fs.writeFileSync('./dist/image.png', buffer);
