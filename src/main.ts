3c1a751c481e7f386db848d46a0ec0dfdd895b66
shamil8 <qurbonovshamil@gmail.com>
2022-09-10 16:16:34 +0700

    Add blue square

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,15 @@ const height = 600;
 const canvas = createCanvas(width, height);
 const context = canvas.getContext('2d');
 
-context.fillStyle = 'blue';
+context.fillStyle = '#b7b7b7';
 context.fillRect(0, 0, width, height);
+
+context.fillStyle = 'yellow';
+
+context.fillRect(0, 0, 150, 100);
+
+context.fillStyle = 'blue';
+context.fillRect(150 + 20, 0, 150, 100);
 
 const buffer = canvas.toBuffer('image/png');
 
